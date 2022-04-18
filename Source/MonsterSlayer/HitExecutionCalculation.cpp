b7deaf9488c42dc5d94688674e795b45240f1b64
Sisipun <sisipun@gmail.com>
2022-04-18 20:16:45 +0300

    Hit gameplay effect from modifiers to custom execution which use weapon power

---
+++ Source/MonsterSlayer/HitExecutionCalculation.cpp
@@ -0,0 +1,51 @@
+#include "HitExecutionCalculation.h"
+#include "CharacterAttributeSet.h"
+#include "BaseCharacter.h"
+
+struct HitExecutionCapture
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Health);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
+
+	HitExecutionCapture()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSet, Health, Target, true);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSet, AttackPower, Source, true);
+	}
+};
+
+static HitExecutionCapture& GetHitExecutionCapture()
+{
+	static HitExecutionCapture Capture;
+	return Capture;
+}
+
+UHitExecutionCalculation::UHitExecutionCalculation()
+{
+	RelevantAttributesToCapture.Add(GetHitExecutionCapture().HealthDef);
+	RelevantAttributesToCapture.Add(GetHitExecutionCapture().AttackPowerDef);
+}
+
+void UHitExecutionCalculation::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& Params, FGameplayEffectCustomExecutionOutput& Output) const
+{
+	float HealthMagnitude = 0.0f;
+	float AttackPowerMagnitude = 0.0f;
+
+	Params.AttemptCalculateCapturedAttributeMagnitude(GetHitExecutionCapture().HealthDef, FAggregatorEvaluateParameters(), HealthMagnitude);
+	Params.AttemptCalculateCapturedAttributeMagnitude(GetHitExecutionCapture().AttackPowerDef, FAggregatorEvaluateParameters(), AttackPowerMagnitude);
+
+	if (Params.GetSourceAbilitySystemComponent())
+	{
+		ABaseCharacter* BaseCharacter = Cast<ABaseCharacter>(Params.GetSourceAbilitySystemComponent()->AvatarActor);
+		if (BaseCharacter->CurrentWeapon)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("EXECUTE"));
+
+			AttackPowerMagnitude += BaseCharacter->CurrentWeapon->Power;
+		}
+	}
+
+	HealthMagnitude -= AttackPowerMagnitude;
+
+	Output.AddOutputModifier(FGameplayModifierEvaluatedData(GetHitExecutionCapture().HealthProperty, EGameplayModOp::Override, HealthMagnitude));
+}
---
+++ Source/MonsterSlayer/HitExecutionCalculation.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "HitExecutionCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONSTERSLAYER_API UHitExecutionCalculation : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UHitExecutionCalculation();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& Params, FGameplayEffectCustomExecutionOutput& Output) const override;
+	
+};
